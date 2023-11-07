de79c4ac53ad484f792f46133d6b9e6189135fbd
Artur Todeschni Crestani <artodeschini@gmail.com>
2023-11-07 20:13:29 -0300

    add WebAppConfit rota para / redirecionar para index.xhtml

---
+++ src/main/java/org/todeschini/contas/WebAppConfig.java
@@ -0,0 +1,17 @@
+package org.todeschini.contas;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebAppConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addRedirectViewController("/", "index.xhtml");
+    }
+
+}
